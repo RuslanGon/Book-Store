1abf2fd42e507acd3d60e998e67fd64def06c7c6
RuslanGon <ruslan441006@gmail.com>
2025-04-03 18:35:30 +0300

    wwer

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import css from './Login.module.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className={css.login_page}>
+      <div className={css.login_container}>
+        <h2>Login</h2> <br />
+        <div className={css.form_group}>
+          <label>UserName:</label>
+          <input type="text" placeholder='userName' />
+        </div>
+        <div className={css.form_group}>
+          <label>Password:</label>
+          <input type="password" placeholder='password' />
+        </div>
+        <div className={css.form_group}>
+          <label>Role:</label>
+          <select name="role" id="role">
+            <option value="admin">Admin</option>
+            <option value="admin">Student</option>
+          </select>
+        </div>
+      </div>
+    </div>
   )
 }
 
