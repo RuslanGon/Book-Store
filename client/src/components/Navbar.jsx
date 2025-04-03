044d75d4de750b865fd63bd9f92e018dc5903b60
RuslanGon <ruslan441006@gmail.com>
2025-04-03 13:54:41 +0300

    wer

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-<nav className='navbar'>
+    <nav className="navbar">
+      <div className="left-navbar">
+        <span>Book Store</span>
+      </div>
+      <div className="right-navbar">
+        <Link className="navbar-link" to='/books'>Books</Link>
+        <Link className="navbar-link" to='/login'>Login</Link>
 
-</nav>
-  )
-}
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
