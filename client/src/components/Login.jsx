4a08bca0e57872c466768ae58f6013805095b61c
RuslanGon <ruslan441006@gmail.com>
2025-04-03 19:03:55 +0300

    wer

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from './Login.module.css'
 
 const Login = () => {
+
+const [userName, setUserNAme] = useState('')  
+const [password, setPassword] = useState('')  
+const [role, setRole] = useState('student')  
+
+const handleSubmit = () => {
+
+}
+
   return (
     <div className={css.login_page}>
       <div className={css.login_container}>
         <div className={css.form_group}>
           <label>UserName:</label>
-          <input type="text" placeholder='userName' />
+          <input type="text" placeholder='userName'  onChange={e => setUserNAme(e.target.value)}/>
         </div>
         <div className={css.form_group}>
           <label>Password:</label>
-          <input type="password" placeholder='password' />
+          <input type="password" placeholder='password' onChange={e => setPassword(e.target.value)} />
         </div>
         <div className={css.form_group}>
           <label>Role:</label>
-          <select name="role" id="role">
+          <select name="role" id="role" onChange={e => setRole(e.target.value)}>
             <option value="admin">Admin</option>
             <option value="student">Student</option>
           </select>
         </div>
-        <button className={css.btn_login}>Login</button>
+        <button className={css.btn_login} onClick={handleSubmit}>Login</button>
       </div>
     </div>
   )
