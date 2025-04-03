90ee49f251cac37f4095d9e31f0808bd5f4f0c4e
RuslanGon <ruslan441006@gmail.com>
2025-04-03 19:45:22 +0300

    wer

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import startServer from './db.js';
 
 
 const app = express()
+startServer();
 dotenv.config()
 
 app.listen(process.env.PORT, () => {
