b99a1a4485b8ab6682deadae4e0fb5907a6fbec2
RuslanGon <ruslan441006@gmail.com>
2025-04-03 20:11:27 +0300

    wer

--- server/index.js
+++ server/index.js
@@ -2,10 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import startServer from './db.js';
 
-dotenv.config();  // Сначала вызываем dotenv.config() для загрузки переменных окружения
+dotenv.config(); 
 
 const app = express();
 
-// Передаем `app` в функцию startServer для подключения к базе данных и запуска сервера
 startServer(app);
 
