e7b34b5ab8ccd3596d3da838493ff82c3b0b25d9
RuslanGon <ruslan441006@gmail.com>
2025-04-08 17:41:56 +0300

    wer

--- client/src/components/LogOut.jsx
+++ client/src/components/LogOut.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const LogOut = () => {
-  return (
-    <div>LogOut</div>
-  )
-}
+  const navigate = useNavigate(); 
 
-export default LogOut
+  useEffect(() => {
+    const logout = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/auth/logout");
+        console.log(response.data); 
+        navigate("/login"); 
+      } catch (error) {
+        console.error("Error during logout:", error);
+      }
+    };
+    
+    logout();
+  }, [navigate]);
+};
+
+export default LogOut;
