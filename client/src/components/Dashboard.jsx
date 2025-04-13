f6f8f5197b3ce9d24463747666012354fbd2bd0e
RuslanGon <ruslan441006@gmail.com>
2025-04-13 10:43:28 +0300

    wer

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import css from './Dashboard.module.css'
+
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className={css.dashboard}>
+      <div className={css.dashboard_box}>
+        <h2>Total books</h2> <br />
+        <h2></h2>
+      </div>
+      <div className={css.dashboard_box}>
+        <h2>Total students</h2> <br />
+        <h2></h2>
+      </div>
+      <div className={css.dashboard_box}>
+        <h2>Total Admins</h2> <br />
+        <h2></h2>
+      </div>
+
+    </div>
   )
 }
 
